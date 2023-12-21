3fb7877a4db00c47063c4865e28008c43e142f04
digiwand <20778143+digiwand@users.noreply.github.com>
2023-12-21 14:32:49 -0800

    feat: create Developer Options Advanced Setting

--- ui/helpers/constants/settings.js
+++ ui/helpers/constants/settings.js
@@ -8,6 +8,7 @@ import {
   NETWORKS_ROUTE,
   CONTACT_LIST_ROUTE,
   EXPERIMENTAL_ROUTE,
+  DEVELOPER_OPTIONS_ROUTE,
 } from './routes';
 
 export const SETTINGS_CONSTANTS = [
@@ -352,11 +353,28 @@ export const SETTINGS_CONSTANTS = [
     route: `${ADVANCED_ROUTE}#restore-userdata`,
     icon: 'fas fa-upload',
   },
+
   {
     tabMessage: (t) => t('experimental'),
     sectionMessage: (t) => t('securityAlerts'),
     descriptionMessage: (t) => t('securityAlertsDescription'),
     route: `${EXPERIMENTAL_ROUTE}#security-alerts`,
     icon: 'fa fa-flask',
+  },
+
+  {
+    tabMessage: (t) => t('developerOptions'),
+    sectionMessage: (t) => t('resetStates'),
+    descriptionMessage: (t) => t('resetStates'),
+    route: `${DEVELOPER_OPTIONS_ROUTE}#reset-states`,
+    icon: IconName.CodeCircle,
+  },
+  {
+    tabMessage: (t) => t('developerOptions'),
+    sectionMessage: (t) => t('announcements'),
+    descriptionMessage: (t) =>
+      t('developerOptionsResetStatesAnnouncementsDescription'),
+    route: `${DEVELOPER_OPTIONS_ROUTE}#reset-states-announcements`,
+    icon: IconName.CodeCircle,
   },
 ];
