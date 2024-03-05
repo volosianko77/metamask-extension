901881ae9550705e4ae418a427ace5d95c66f8a5
David Walsh <davidwalsh83@gmail.com>
2024-03-05 12:41:11 -0600

    Implement switched network toast

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -103,7 +103,6 @@ import {
   SURVEY_END_TIME,
   SURVEY_START_TIME,
 } from '../helpers/constants/survey';
-import state from '../../.storybook/test-data';
 import {
   getCurrentNetworkTransactions,
   getUnapprovedTransactions,
@@ -617,11 +616,12 @@ export function getGasIsLoading(state) {
 }
 
 export function getNeverShowSwitchedNetworkMessage(state) {
-  return state.appState.switchedNetworkNeverShowMessage;
+  return state.metamask.switchedNetworkNeverShowMessage;
 }
 
 export function getSwitchedNetworkDetails(state) {
-  return state.appState.switchedNetworkDetails;
+  console.log('getSwitchedNetworkDetails; appState is: ', state.appState);
+  return state.metamask.switchedNetworkDetails;
 }
 
 export function getAppIsLoading(state) {
@@ -1127,7 +1127,7 @@ export function getShowWhatsNewPopup(state) {
 }
 
 export function getCurrentPopupId(state) {
-  console.log("App state is: ", state.appState);
+  console.log('App state is: ', state.appState);
   return state.appState.currentPopupId;
 }
 
